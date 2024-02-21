52e0562027f8c62456ed1376cd29a66e361dc0fb
Jonnadula Chaitanya <sparkscj234@gmail.com>
2024-02-21 22:07:41 +0530

    boiler plate code added

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req,res) => {
+    res.json({
+        message: "🚀Welcome to code converter service🚀"
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
